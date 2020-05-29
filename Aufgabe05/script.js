f52831426f34068a36f878d47bd0d13a508c93ba
danielmeisler <daniel_meisler@web.de>
2020-05-29 15:16:00 +0200

    Update

--- Aufgabe05/script.js
+++ Aufgabe05/script.js
@@ -3,43 +3,45 @@ var Aufgabe05;
 (function (Aufgabe05) {
     for (let i = 0; i < Aufgabe05.waffel.length; i++) {
         let xDiv = document.createElement("div");
-        xDiv.id = "divNr" + i;
+        xDiv.id = "waffelDiv" + i;
+        xDiv.classList.add("waffelClass");
         document.getElementById("cw")?.appendChild(xDiv);
         let xImg = document.createElement("img");
         xImg.src = Aufgabe05.waffel[i].bild;
-        document.getElementById("divNr" + i)?.appendChild(xImg);
+        document.getElementById("waffelDiv" + i)?.appendChild(xImg);
         let xH2 = document.createElement("h2");
         xH2.innerHTML = Aufgabe05.waffel[i].titel;
-        document.getElementById("divNr" + i)?.appendChild(xH2);
+        document.getElementById("waffelDiv" + i)?.appendChild(xH2);
         let xP = document.createElement("p");
         xP.innerHTML = Aufgabe05.waffel[i].text;
-        document.getElementById("divNr" + i)?.appendChild(xP);
+        document.getElementById("waffelDiv" + i)?.appendChild(xP);
         let xH3 = document.createElement("h3");
         xH3.innerHTML = Aufgabe05.waffel[i].preis;
-        document.getElementById("divNr" + i)?.appendChild(xH3);
+        document.getElementById("waffelDiv" + i)?.appendChild(xH3);
         let xButton = document.createElement("button");
         xButton.innerHTML = "In den Warenkorb";
-        document.getElementById("divNr" + i)?.appendChild(xButton);
+        document.getElementById("waffelDiv" + i)?.appendChild(xButton);
     }
     for (let i = 0; i < Aufgabe05.becher.length; i++) {
         let xDiv = document.createElement("div");
-        xDiv.id = "divNr2" + i;
+        xDiv.id = "becherDiv" + i;
+        xDiv.classList.add("becherClass");
         document.getElementById("cb")?.appendChild(xDiv);
         let xImg = document.createElement("img");
         xImg.src = Aufgabe05.becher[i].bild;
-        document.getElementById("divNr2" + i)?.appendChild(xImg);
+        document.getElementById("becherDiv" + i)?.appendChild(xImg);
         let xH2 = document.createElement("h2");
         xH2.innerHTML = Aufgabe05.becher[i].titel;
-        document.getElementById("divNr2" + i)?.appendChild(xH2);
+        document.getElementById("becherDiv" + i)?.appendChild(xH2);
         let xP = document.createElement("p");
         xP.innerHTML = Aufgabe05.becher[i].text;
-        document.getElementById("divNr2" + i)?.appendChild(xP);
+        document.getElementById("becherDiv" + i)?.appendChild(xP);
         let xH3 = document.createElement("h3");
         xH3.innerHTML = Aufgabe05.becher[i].preis;
-        document.getElementById("divNr2" + i)?.appendChild(xH3);
+        document.getElementById("becherDiv" + i)?.appendChild(xH3);
         let input = document.createElement("button");
         input.innerHTML = "In den Warenkorb";
-        document.getElementById("divNr2" + i)?.appendChild(input);
+        document.getElementById("becherDiv" + i)?.appendChild(input);
     }
 })(Aufgabe05 || (Aufgabe05 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe05/script.ts
+++ Aufgabe05/script.ts
@@ -3,55 +3,57 @@ namespace Aufgabe05 {
 for (let i: number = 0; i < waffel.length; i++) {
   
     let xDiv: HTMLDivElement = document.createElement("div");
-    xDiv.id = "divNr" + i;
+    xDiv.id = "waffelDiv" + i;
+    xDiv.classList.add("waffelClass");
     document.getElementById("cw")?.appendChild(xDiv);
 
     let xImg: HTMLImageElement = document.createElement("img");
     xImg.src = waffel[i].bild;
-    document.getElementById("divNr" + i)?.appendChild(xImg);
+    document.getElementById("waffelDiv" + i)?.appendChild(xImg);
 
     let xH2: HTMLElement = document.createElement("h2");
     xH2.innerHTML = waffel[i].titel;
-    document.getElementById("divNr" + i)?.appendChild(xH2);
+    document.getElementById("waffelDiv" + i)?.appendChild(xH2);
 
     let xP: HTMLParagraphElement = document.createElement("p");
     xP.innerHTML = waffel[i].text;
-    document.getElementById("divNr" + i)?.appendChild(xP);
+    document.getElementById("waffelDiv" + i)?.appendChild(xP);
 
     let xH3: HTMLElement = document.createElement("h3");
     xH3.innerHTML = waffel[i].preis;
-    document.getElementById("divNr" + i)?.appendChild(xH3);
+    document.getElementById("waffelDiv" + i)?.appendChild(xH3);
 
     let xButton: HTMLButtonElement = document.createElement("button");
     xButton.innerHTML = "In den Warenkorb";
-    document.getElementById("divNr" + i)?.appendChild(xButton);
+    document.getElementById("waffelDiv" + i)?.appendChild(xButton);
 
 }
 for (let i: number = 0; i < becher.length; i++) {
   
     let xDiv: HTMLDivElement = document.createElement("div");
-    xDiv.id = "divNr2" + i;
+    xDiv.id = "becherDiv" + i;
+    xDiv.classList.add("becherClass");
     document.getElementById("cb")?.appendChild(xDiv);
 
     let xImg: HTMLImageElement = document.createElement("img");
     xImg.src = becher[i].bild;
-    document.getElementById("divNr2" + i)?.appendChild(xImg);
+    document.getElementById("becherDiv" + i)?.appendChild(xImg);
 
     let xH2: HTMLElement = document.createElement("h2");
     xH2.innerHTML = becher[i].titel;
-    document.getElementById("divNr2" + i)?.appendChild(xH2);
+    document.getElementById("becherDiv" + i)?.appendChild(xH2);
 
     let xP: HTMLParagraphElement = document.createElement("p");
     xP.innerHTML = becher[i].text;
-    document.getElementById("divNr2" + i)?.appendChild(xP);
+    document.getElementById("becherDiv" + i)?.appendChild(xP);
 
     let xH3: HTMLElement = document.createElement("h3");
     xH3.innerHTML = becher[i].preis;
-    document.getElementById("divNr2" + i)?.appendChild(xH3);
+    document.getElementById("becherDiv" + i)?.appendChild(xH3);
 
     let input: HTMLButtonElement = document.createElement("button");
     input.innerHTML = "In den Warenkorb";
-    document.getElementById("divNr2" + i)?.appendChild(input);
+    document.getElementById("becherDiv" + i)?.appendChild(input);
 
 }
 

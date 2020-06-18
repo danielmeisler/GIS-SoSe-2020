11eaa8b87db60aaf63e2515dab7871015db20607
danielmeisler <daniel_meisler@web.de>
2020-06-18 13:48:14 +0200

    Update

---
+++ Aufgabe08/server.js
@@ -0,0 +1,18 @@
+"use strict";
+var Aufgabe08;
+(function (Aufgabe08) {
+    document.getElementById("buttonID")?.addEventListener("click", handleButton);
+    function handleButton() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://gissose2020-danielmeisler.herokuapp.com";
+        let query = new URLSearchParams(formData);
+        url = url + "?" + query.toString();
+        communicate(url);
+    }
+    async function communicate(_url) {
+        let response = await fetch(_url, { method: "get" });
+        let response2 = await response.text();
+        console.log("Response", response2);
+    }
+})(Aufgabe08 || (Aufgabe08 = {}));
+//# sourceMappingURL=server.js.map
---
+++ Aufgabe08/server.ts
@@ -0,0 +1,19 @@
+namespace Aufgabe08 {
+
+    document.getElementById("buttonID")?.addEventListener("click", handleButton);
+
+    function handleButton(): void {
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://gissose2020-danielmeisler.herokuapp.com";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+        communicate(url);
+    } 
+
+    async function communicate(_url: RequestInfo): Promise<void> {
+        let response: Response = await fetch(_url, { method: "get" });
+        let response2: String = await response.text();
+        console.log("Response", response2);
+      }
+
+}
